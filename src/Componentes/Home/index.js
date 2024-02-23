43803a8c38eb2309ed5324d2bef252b19666d332
Diogo Kimisima <132942700+diogokimisima@users.noreply.github.com>
2024-02-23 15:44:21 -0300

    atualizado

--- src/Componentes/Home/index.js
+++ src/Componentes/Home/index.js
@@ -1,19 +1,27 @@
 import './home.css';
 import fotoBanner from '../../img/FotoBanner.jpg';
-import iconeCurriculo from '../../img/icone-curriculo.jpg';
-import iconePerfil from '../../img/icone-linkedin.png';
+import iconeCurriculo from '../../img/icone-curriculo.png';
+import iconePerfil from '../../img/icone-linkedin.jpg';
 
 export default function Home() {
     return (
         <section className='limitar-secao'>
             <div className='secao-home'>
                 <div className='descricao-home'>
-                    <h1>Seja bem vindo!</h1>
-                    <h2>Meu nome é Diogo</h2>
+                    <h2>Olá, sou o Diogo!</h2>
                     <span className='span'>| Front-End Developer</span>
+                    <p>Olá, seja bem vindo(a)! Prepadado(a) para uma experiência arrebatadora? Prazer, sou desenvolvedor Front-end. Fique a vontade para conhecer melhor o meu trabalho.</p>
                     <div className='secao-link'>
-                        <a className='link' href="/" ><img src={iconeCurriculo} alt="curriculo"/>Currículo</a>
+                        <a className='link' href="/" ><img src={iconeCurriculo} alt="curriculo"/>Baixar CV</a>
                         <a className='link' href="https://www.linkedin.com/in/diogo-kimisima/" target='blank' ><img src={iconePerfil} alt="perfil"/>Meu Perfil</a>
+                    </div>
+                    <div className='secao-redes'>
+                        <p>Minhas redes: </p>
+                        <div>
+                            <a><img src="" alt="linkedin"/></a>
+                            <a><img src="" alt="github"/></a>
+                            <a><img src="" alt="instagram"/></a>
+                        </div>
                     </div>
                     
                 </div>
--- src/Componentes/Topo/index.js
+++ src/Componentes/Topo/index.js
@@ -5,13 +5,19 @@ export default function Topo() {
         <header>
             <div className='limitar-secao navbar'>
                 <div className='nome-topo'>
-                    DIOGO DEV
+                    <h1>Diogo kimisima</h1>
+                    <h2>Front-End Developer</h2>
                 </div>
                 <nav>
                     <div>
-                        <a href='/' className='link-topo' to="/" >HOME</a>
-                        <a href='/' className='link-topo' to="/Projetos">PROJETOS</a>
-                        <a href='/' className='link-topo' to="/Sobre">SOBRE</a>
+                        <a href='/' className='link-topo' to="/" >Home</a>
+                        <a href='/' className='link-topo' to="/Sobre">Sobre</a>
+                        <a href='/' className='link-topo' to="/Sobre">Skills</a>
+                        <a href='/' className='link-topo' to="/Projetos">Educação</a>
+                        <a href='/' className='link-topo' to="/Projetos">Projetos</a>
+                        <a href='/' className='link-topo' to="/Projetos">Contato</a>
+
+                        
                     </div>
                 </nav>
             </div>
