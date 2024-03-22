ea663edd0ea6cd06a80c87fa8441ea2477a54e50
dkimisima <dkimisima@gmail.com>
2024-03-22 15:55:27 -0300

    fazendo a secao de contato

--- src/Componentes/Contato/index.jsx
+++ src/Componentes/Contato/index.jsx
@@ -3,7 +3,11 @@ import './contato.css';
 export default function Contato() {
     return (
        <section>
-        
+            <div>
+                <form action="">
+                    
+                </form>
+            </div>
        </section>
     )
 }
